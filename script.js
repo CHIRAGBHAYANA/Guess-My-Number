2997293560952482f8b6eca35410a02268fcb526
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2021-10-13 00:47:38 +0530

    Implemented the high Score Functionalitiy

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 "use strict";
-const correctNumber = Math.round(Math.random() * 20);
+let correctNumber = Math.round(Math.random() * 20);
 console.log(correctNumber);
 let score = 20;
+let highScore = document.querySelector(".highscore").textContent;
+console.log(highScore);
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   if (!guess) {
@@ -30,11 +32,20 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = correctNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number ").style.width = "30rem";
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
   }
 });
 document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  correctNumber = Math.round(Math.random() * 20);
+  console.log(correctNumber);
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".message").textContent = "Start guessing...";
-  document.querySelector(".score").textContent = "20";
+  document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".number ").style.width = "15rem";
 });
