96a631865816980849705e923edb4e2a17e31498
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2021-10-14 01:22:21 +0530

    Refactor the code

--- script.js
+++ script.js
@@ -3,38 +3,54 @@ let correctNumber = Math.round(Math.random() * 20);
 console.log(correctNumber);
 let score = 20;
 let highScore = document.querySelector(".highscore").textContent;
-console.log(highScore);
+const displayMessage = (message) => {
+  document.querySelector(".message").textContent = message;
+};
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   if (!guess) {
     document.querySelector(".message").textContent = "No number";
   }
-  if (guess > correctNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "📈 Too High";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "🎇You loose the game";
-      document.querySelector(".score").textContent = 0;
-    }
-  } else if (correctNumber > guess) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "📉 Too Low";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "🎇You loose the game";
-      document.querySelector(".score").textContent = 0;
-    }
-  } else if (correctNumber === guess) {
-    document.querySelector(".message").textContent = "🎉 Correct ans";
+  // if (guess > correctNumber) {
+  //   if (score > 1) {
+  //     document.querySelector(".message").textContent = "📈 Too High";
+  //     score--;
+  //     document.querySelector(".score").textContent = score;
+  //   } else {
+  //     document.querySelector(".message").textContent = "🎇You loose the game";
+  //     document.querySelector(".score").textContent = 0;
+  //   }
+  // } else if (correctNumber > guess) {
+  //   if (score > 1) {
+  //     document.querySelector(".message").textContent = "📉 Too Low";
+  //     score--;
+  //     document.querySelector(".score").textContent = score;
+  //   } else {
+  //     document.querySelector(".message").textContent = "🎇You loose the game";
+  //     document.querySelector(".score").textContent = 0;
+  //   }
+  // }
+  if (correctNumber === guess) {
+    // document.querySelector(".message").textContent = "🎉 Correct ans";
+    displayMessage("🎉 Correct Answer! ");
     document.querySelector(".number").textContent = correctNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number ").style.width = "30rem";
     if (score > highScore) {
       highScore = score;
       document.querySelector(".highscore").textContent = highScore;
+    }
+  } else if (correctNumber !== guess) {
+    if (score > 1) {
+      correctNumber > guess
+        ? displayMessage("📈 Too High")
+        : displayMessage("📉 Too Low");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      // document.querySelector(".message").textContent = "🎇You loose the game";
+      displayMessage("🎇You loose the game");
+      document.querySelector(".score").textContent = 0;
     }
   }
 });
@@ -43,7 +59,8 @@ document.querySelector(".again").addEventListener("click", function () {
   correctNumber = Math.round(Math.random() * 20);
   console.log(correctNumber);
   document.querySelector("body").style.backgroundColor = "#222";
-  document.querySelector(".message").textContent = "Start guessing...";
+  // document.querySelector(".message").textContent = "Start guessing...";
+  displayMessage("Start guessing...");
   document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
